393ca7bfcd07afff7a0828718e7c993f348654cf
NoneType4Name <90710372+NoneType4Name@users.noreply.github.com>
2025-12-01 00:00:18 +0300

    initial

---
+++ Core/Src/main.c
@@ -0,0 +1,568 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "fatfs.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "ov2640.h"
+#include "stm32h750xx.h"
+#include "stm32h7xx_hal_gpio.h"
+#include <stdio.h>
+#include <string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+DCMI_HandleTypeDef hdcmi;
+DMA_HandleTypeDef hdma_dcmi;
+
+I2C_HandleTypeDef hi2c1;
+
+SD_HandleTypeDef hsd1;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config( void );
+static void MPU_Config( void );
+static void MX_GPIO_Init( void );
+static void MX_DMA_Init( void );
+static void MX_SDMMC1_SD_Init( void );
+static void MX_I2C1_Init( void );
+/* USER CODE BEGIN PFP */
+
+static void SDMMC_SDCard_Test( void );
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+enum imageResolution imgRes        = RES_640X480;
+uint8_t frameBuffer[ RES_640X480 ] = { 0 };
+unsigned short mutex               = 0;
+uint16_t bufferPointer             = 0;
+unsigned short headerFound         = 0;
+
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main( void )
+{
+    /* USER CODE BEGIN 1 */
+
+    /* USER CODE END 1 */
+
+    /* MPU Configuration--------------------------------------------------------*/
+    MPU_Config();
+
+    /* MCU Configuration--------------------------------------------------------*/
+
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
+
+    /* USER CODE BEGIN Init */
+
+    /* USER CODE END Init */
+
+    /* Configure the system clock */
+    SystemClock_Config();
+
+    /* USER CODE BEGIN SysInit */
+
+    /* USER CODE END SysInit */
+
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_DMA_Init();
+    // MX_SDMMC1_SD_Init();
+    // MX_FATFS_Init();
+    // MX_DCMI_Init();
+    // MX_I2C1_Init();
+    /* USER CODE BEGIN 2 */
+    // OV2640_Init(&hi2c1, &hdcmi);
+    // HAL_Delay(10);
+    // OV2640_ResolutionOptions(imgRes);
+    // HAL_Delay(100);
+    // SDMMC_SDCard_Test();
+    /* USER CODE END 2 */
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while ( 1 )
+    {
+        /*if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13))
+        {
+                memset(frameBuffer, 0, sizeof(frameBuffer));
+                OV2640_CaptureSnapshot((uint32_t) frameBuffer, imgRes);
+                while (1) {
+                                if (headerFound == 0 && frameBuffer[bufferPointer] == 0xFF
+                                                && frameBuffer[bufferPointer + 1] == 0xD8) {
+                                        headerFound = 1;
+                                }
+                                if (headerFound == 1 && frameBuffer[bufferPointer] == 0xFF
+                                                && frameBuffer[bufferPointer + 1] == 0xD9) {
+                                        bufferPointer = bufferPointer + 2;
+                                        headerFound = 0;
+                                        break;
+                                }
+
+                                if (bufferPointer >= 65535) {
+                                        break;
+                                }
+                                bufferPointer++;
+                        }
+                        FATFS FatFs;
+                        FIL Fil;
+                        FRESULT FR_Status;
+                        UINT WWC; // Read/Write Word Counter
+                                                        char RW_Buffer[200];
+                        FATFS *FS_Ptr;
+                        do
+                        {
+                                FR_Status = f_mount(&FatFs, SDPath, 1);
+                                if (FR_Status != FR_OK)
+                                {
+                                        break;
+                                }
+                                FR_Status = f_open(&Fil, "photo.jpeg", FA_WRITE | FA_CREATE_ALWAYS);
+                                if(FR_Status != FR_OK)
+                                {
+                                        break;
+                                }
+                                strcpy(RW_Buffer, "Hello! From STM32 To SD Card Over SDMMC, Using f_write()\r\n");
+                                f_write(&Fil, RW_Buffer, strlen(RW_Buffer), &WWC);
+                        }
+                        while(0);*/
+        //}
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
+    }
+    /* USER CODE END 3 */
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config( void )
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+    /** Supply configuration update enable
+     */
+    HAL_PWREx_ConfigSupply( PWR_LDO_SUPPLY );
+
+    /** Configure the main internal regulator output voltage
+     */
+    __HAL_PWR_VOLTAGESCALING_CONFIG( PWR_REGULATOR_VOLTAGE_SCALE3 );
+
+    while ( !__HAL_PWR_GET_FLAG( PWR_FLAG_VOSRDY ) )
+    {
+    }
+
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState            = RCC_HSE_ON;
+    RCC_OscInitStruct.HSIState            = RCC_HSI_DIV1;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM            = 2;
+    RCC_OscInitStruct.PLL.PLLN            = 20;
+    RCC_OscInitStruct.PLL.PLLP            = 2;
+    RCC_OscInitStruct.PLL.PLLQ            = 4;
+    RCC_OscInitStruct.PLL.PLLR            = 2;
+    RCC_OscInitStruct.PLL.PLLRGE          = RCC_PLL1VCIRANGE_3;
+    RCC_OscInitStruct.PLL.PLLVCOSEL       = RCC_PLL1VCOWIDE;
+    RCC_OscInitStruct.PLL.PLLFRACN        = 0;
+    if ( HAL_RCC_OscConfig( &RCC_OscInitStruct ) != HAL_OK )
+    {
+        Error_Handler();
+    }
+
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.SYSCLKDivider  = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
+    RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
+
+    if ( HAL_RCC_ClockConfig( &RCC_ClkInitStruct, FLASH_LATENCY_2 ) != HAL_OK )
+    {
+        Error_Handler();
+    }
+    HAL_RCC_MCOConfig( RCC_MCO1, RCC_MCO1SOURCE_HSI, RCC_MCODIV_1 );
+}
+
+/**
+ * @brief DCMI Initialization Function
+ * @param None
+ * @retval None
+ */
+void MX_DCMI_Init( void )
+{
+    /* USER CODE BEGIN DCMI_Init 0 */
+
+    /* USER CODE END DCMI_Init 0 */
+
+    /* USER CODE BEGIN DCMI_Init 1 */
+
+    /* USER CODE END DCMI_Init 1 */
+    hdcmi.Instance              = DCMI;
+    hdcmi.Init.SynchroMode      = DCMI_SYNCHRO_HARDWARE;
+    hdcmi.Init.PCKPolarity      = DCMI_PCKPOLARITY_RISING;
+    hdcmi.Init.VSPolarity       = DCMI_VSPOLARITY_LOW;
+    hdcmi.Init.HSPolarity       = DCMI_HSPOLARITY_LOW;
+    hdcmi.Init.CaptureRate      = DCMI_CR_ALL_FRAME;
+    hdcmi.Init.ExtendedDataMode = DCMI_EXTEND_DATA_8B;
+    hdcmi.Init.JPEGMode         = DCMI_JPEG_ENABLE;
+    hdcmi.Init.ByteSelectMode   = DCMI_BSM_ALL;
+    hdcmi.Init.ByteSelectStart  = DCMI_OEBS_ODD;
+    hdcmi.Init.LineSelectMode   = DCMI_LSM_ALL;
+    hdcmi.Init.LineSelectStart  = DCMI_OELS_ODD;
+    if ( HAL_DCMI_Init( &hdcmi ) != HAL_OK )
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN DCMI_Init 2 */
+
+    /* USER CODE END DCMI_Init 2 */
+}
+
+/**
+ * @brief I2C1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_I2C1_Init( void )
+{
+    /* USER CODE BEGIN I2C1_Init 0 */
+
+    /* USER CODE END I2C1_Init 0 */
+
+    /* USER CODE BEGIN I2C1_Init 1 */
+
+    /* USER CODE END I2C1_Init 1 */
+    hi2c1.Instance              = I2C1;
+    hi2c1.Init.Timing           = 0x00C0EAFF;
+    hi2c1.Init.OwnAddress1      = 0;
+    hi2c1.Init.AddressingMode   = I2C_ADDRESSINGMODE_7BIT;
+    hi2c1.Init.DualAddressMode  = I2C_DUALADDRESS_DISABLE;
+    hi2c1.Init.OwnAddress2      = 0;
+    hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+    hi2c1.Init.GeneralCallMode  = I2C_GENERALCALL_DISABLE;
+    hi2c1.Init.NoStretchMode    = I2C_NOSTRETCH_DISABLE;
+    if ( HAL_I2C_Init( &hi2c1 ) != HAL_OK )
+    {
+        Error_Handler();
+    }
+
+    /** Configure Analogue filter
+     */
+    if ( HAL_I2CEx_ConfigAnalogFilter( &hi2c1, I2C_ANALOGFILTER_ENABLE ) != HAL_OK )
+    {
+        Error_Handler();
+    }
+
+    /** Configure Digital filter
+     */
+    if ( HAL_I2CEx_ConfigDigitalFilter( &hi2c1, 0 ) != HAL_OK )
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN I2C1_Init 2 */
+
+    /* USER CODE END I2C1_Init 2 */
+}
+
+/**
+ * @brief SDMMC1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_SDMMC1_SD_Init( void )
+{
+    /* USER CODE BEGIN SDMMC1_Init 0 */
+
+    /* USER CODE END SDMMC1_Init 0 */
+
+    /* USER CODE BEGIN SDMMC1_Init 1 */
+
+    /* USER CODE END SDMMC1_Init 1 */
+    hsd1.Instance                 = SDMMC1;
+    hsd1.Init.ClockEdge           = SDMMC_CLOCK_EDGE_RISING;
+    hsd1.Init.ClockPowerSave      = SDMMC_CLOCK_POWER_SAVE_DISABLE;
+    hsd1.Init.BusWide             = SDMMC_BUS_WIDE_4B;
+    hsd1.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_DISABLE;
+    hsd1.Init.ClockDiv            = 2;
+    if ( HAL_SD_Init( &hsd1 ) != HAL_OK )
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN SDMMC1_Init 2 */
+
+    /* USER CODE END SDMMC1_Init 2 */
+}
+
+/**
+ * Enable DMA controller clock
+ */
+static void MX_DMA_Init( void )
+{
+    /* DMA controller clock enable */
+    __HAL_RCC_DMA1_CLK_ENABLE();
+
+    /* DMA interrupt init */
+    /* DMA1_Stream1_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority( DMA1_Stream1_IRQn, 0, 0 );
+    HAL_NVIC_EnableIRQ( DMA1_Stream1_IRQn );
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init( void )
+{
+    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+    /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+    /* USER CODE END MX_GPIO_Init_1 */
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin( LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET );
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin( DCMI_PWDN_GPIO_Port, DCMI_PWDN_Pin, GPIO_PIN_RESET );
+
+    /*Configure GPIO pin : LED_Pin */
+    GPIO_InitStruct.Pin   = LED_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init( LED_GPIO_Port, &GPIO_InitStruct );
+
+    /*Configure GPIO pin : BUTTON_Pin */
+    GPIO_InitStruct.Pin  = BUTTON_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    HAL_GPIO_Init( BUTTON_GPIO_Port, &GPIO_InitStruct );
+
+    /*Configure GPIO pin : DCMI_PWDN_Pin */
+    GPIO_InitStruct.Pin   = DCMI_PWDN_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init( DCMI_PWDN_GPIO_Port, &GPIO_InitStruct );
+
+    /*Configure GPIO pin : DCMI_XCLK_Pin */
+    GPIO_InitStruct.Pin       = DCMI_XCLK_Pin;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull      = GPIO_NOPULL;
+    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF0_MCO;
+    HAL_GPIO_Init( DCMI_XCLK_GPIO_Port, &GPIO_InitStruct );
+
+    /*Configure GPIO pin : SDMMC1_SW_Pin */
+    GPIO_InitStruct.Pin  = SDMMC1_SW_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    HAL_GPIO_Init( SDMMC1_SW_GPIO_Port, &GPIO_InitStruct );
+
+    /* EXTI interrupt init*/
+    HAL_NVIC_SetPriority( SDMMC1_SW_EXTI_IRQn, 0, 0 );
+    HAL_NVIC_EnableIRQ( SDMMC1_SW_EXTI_IRQn );
+
+    /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+    /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+void HAL_GPIO_EXTI_Callback( uint16_t GPIO_Pin )
+{
+    if ( GPIO_Pin == GPIO_PIN_4 )
+    {
+        __HAL_GPIO_EXTI_CLEAR_FLAG( GPIO_PIN_0 );
+        if ( HAL_GPIO_ReadPin( SDMMC1_SW_GPIO_Port, SDMMC1_SW_Pin ) )
+        {
+            HAL_GPIO_WritePin( GPIOE, GPIO_PIN_3, GPIO_PIN_SET );
+        }
+        else
+            HAL_GPIO_WritePin( GPIOE, GPIO_PIN_3, GPIO_PIN_RESET );
+    }
+    else
+    {
+        __NOP();
+    }
+}
+
+static void SDMMC_SDCard_Test( void )
+{
+    FATFS FatFs;
+    FIL Fil;
+    FRESULT FR_Status;
+    FATFS *FS_Ptr;
+    DWORD FreeClusters;
+    UINT RWC, WWC; // Read/Write Word Counter
+    unsigned long TotalSize, FreeSpace;
+    char RW_Buffer[ 200 ];
+    do
+    {
+        //------------------[ Mount The SD Card ]--------------------
+        FR_Status = f_mount( &FatFs, SDPath, 1 );
+        if ( FR_Status != FR_OK )
+        {
+            break;
+        }
+        //------------------[ Get & Print The SD Card Size & Free Space ]--------------------
+        f_getfree( "", &FreeClusters, &FS_Ptr );
+        TotalSize = ( uint32_t ) ( ( FS_Ptr->n_fatent - 2 ) * FS_Ptr->csize * 0.5 );
+        FreeSpace = ( uint32_t ) ( FreeClusters * FS_Ptr->csize * 0.5 );
+        //------------------[ Open A Text File For Write & Write Data ]--------------------
+        // Open the file
+        FR_Status = f_open( &Fil, "MyTextFile.txt", FA_WRITE | FA_READ | FA_CREATE_ALWAYS );
+        if ( FR_Status != FR_OK )
+        {
+            break;
+        }
+        // (1) Write Data To The Text File [ Using f_puts() Function ]
+        // (2) Write Data To The Text File [ Using f_write() Function ]
+        strcpy( RW_Buffer, "Helo! From STM32 To SD Card Over SDMMC, Using f_write()\r\n" );
+        f_write( &Fil, RW_Buffer, strlen( RW_Buffer ), &WWC );
+        // Close The File
+        f_close( &Fil );
+        //------------------[ Open A Text File For Read & Read Its Data ]--------------------
+        // Open The File
+        //------------------[ Delete The Text File ]--------------------
+        // Delete The File
+        /*
+        FR_Status = f_unlink(MyTextFile.txt);
+        if (FR_Status != FR_OK){
+            printf("Error! While Deleting The (MyTextFile.txt) File.. \r\n");
+        }
+        */
+    } while ( 0 );
+    //------------------[ Test Complete! Unmount The SD Card ]--------------------
+    FR_Status = f_mount( NULL, "", 0 );
+}
+
+/* USER CODE END 4 */
+
+/* MPU Configuration */
+
+void MPU_Config( void )
+{
+    MPU_Region_InitTypeDef MPU_InitStruct = { 0 };
+
+    /* Disables the MPU */
+    HAL_MPU_Disable();
+
+    /** Initializes and configures the Region and the memory to be protected
+     */
+    MPU_InitStruct.Enable           = MPU_REGION_ENABLE;
+    MPU_InitStruct.Number           = MPU_REGION_NUMBER0;
+    MPU_InitStruct.BaseAddress      = 0x0;
+    MPU_InitStruct.Size             = MPU_REGION_SIZE_4GB;
+    MPU_InitStruct.SubRegionDisable = 0x87;
+    MPU_InitStruct.TypeExtField     = MPU_TEX_LEVEL0;
+    MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
+    MPU_InitStruct.DisableExec      = MPU_INSTRUCTION_ACCESS_DISABLE;
+    MPU_InitStruct.IsShareable      = MPU_ACCESS_SHAREABLE;
+    MPU_InitStruct.IsCacheable      = MPU_ACCESS_NOT_CACHEABLE;
+    MPU_InitStruct.IsBufferable     = MPU_ACCESS_NOT_BUFFERABLE;
+
+    HAL_MPU_ConfigRegion( &MPU_InitStruct );
+    /* Enables the MPU */
+    HAL_MPU_Enable( MPU_PRIVILEGED_DEFAULT );
+}
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler( void )
+{
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while ( 1 )
+    {
+        HAL_GPIO_TogglePin( GPIOE, GPIO_PIN_3 );
+        HAL_Delay( 1000 );
+    }
+    /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed( uint8_t *file, uint32_t line )
+{
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line number,
+       //ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
